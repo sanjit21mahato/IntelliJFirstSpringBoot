a7729d9c64fee1417b549ee373a25ce318a48252
sanjit21mahato <sanjit21mahato@gmail.com>
2026-02-03 22:13:00 +0530

    testing

---
+++ src/main/java/com/sam/IntelliJFirstSpringBoot/Test.java
@@ -0,0 +1,17 @@
+package com.sam.IntelliJFirstSpringBoot;
+
+
+public class Test {
+
+    private String testing;
+
+    public String getTesting() {
+        return testing;
+    }
+
+    public void setTesting(String testing) {
+        this.testing = testing;
+    }
+
+
+}
