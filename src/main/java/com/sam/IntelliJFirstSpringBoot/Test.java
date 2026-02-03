58cc89054a2b863024abd557e13843669122625f
sanjit21mahato <sanjit21mahato@gmail.com>
2026-02-03 22:40:38 +0530

    changes in test

--- src/main/java/com/sam/IntelliJFirstSpringBoot/Test.java
+++ src/main/java/com/sam/IntelliJFirstSpringBoot/Test.java
@@ -3,14 +3,14 @@ package com.sam.IntelliJFirstSpringBoot;
 
 public class Test {
 
-    private String testing;
+    private String testin;
 
-    public String getTesting() {
-        return testing;
+    public String getTestin() {
+        return testin;
     }
 
-    public void setTesting(String testing) {
-        this.testing = testing;
+    public void setTestin(String testin) {
+        this.testin = testin;
     }
 
     @Override
